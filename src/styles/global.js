34907a478582917f38508b438bc6f62d0fb9230a
Vinicius Birreik <viniciusbirreik@gmail.com>
2022-10-18 21:13:12 -0300

    adicionando estilo global

---
+++ src/styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+const Global = createGlobalStyle`
+    body {
+        padding: 0;
+        margin: 0;
+        font-family: sans-serif;
+        background-color: #f2f2f2;
+    }
+`;
+
+export default Global;
